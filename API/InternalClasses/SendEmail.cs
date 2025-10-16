605cd033967d67c6e36b4f7fc0a3b8c204781c84
ArtemBruh <104833855+ArtemTop495@users.noreply.github.com>
2025-10-17 00:34:48 +0500

    Upload sendemail class

---
+++ API/InternalClasses/SendEmail.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace API.InternalClasses
+{
+    public static class SendEmail
+    {
+        public static void SendLoginInformation(string email, string password)
+        {
+            MailAddress from = new MailAddress("ksebija@gmail.com", "SkyWhySales");
+            MailAddress to = new MailAddress(email);
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Информация для входа в приложение";
+            m.Body = $"<h2>Логин: {email}</h2><br/><h2>Пароль: {password}</h2>";
+            m.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("ksebija@gmail.com", "brsq xdqx jzce jsqm");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+        }
+    }
+}
