ab8a781502e503e10b9ea34c026ebd63cf3ad138
theaidarmaps <aidarkin1010@gmail.com>
2025-10-20 18:49:31 +0500

    Update certificate script

--- cert.ps1
+++ cert.ps1
@@ -1,2 +1,3 @@
+New-Item -Path "$env:USERPROFILE\.aspnet\https" -ItemType Directory -Force
 dotnet dev-certs https -ep "$env:USERPROFILE\.aspnet\https\aspnetapp.pfx"  -p 1245780r
 dotnet dev-certs https --trust
